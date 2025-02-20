87f81a3515ed8938e8bcbce467fcf6de4e3ea75a
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-20 10:09:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,27 @@
 
 ###❖➥
-API_ID = "123456"  # api id
+API_ID = "23331203"  # api id
 
 ###❖➥
-API_HASH = "ed44780dedd182084f2133b16944cf565"  
+API_HASH = "05be4bb2e1e6806a2ffd23402079e23a"  
 
 ###❖➥
 BOT_TOKEN = None
 
 ###❖➥
-HOST = "localhost"  # redis host uri
+HOST = "redis-cli -u redis://default:ox6hcmlxU3ZezlpjkOCeETnmGTxMoEXl@redis-13316.c84.us-east-1-2.ec2.redns.redis-cloud.com:13316"  # redis host uri
 
 ###❖➥
 PORT = "6379"  # redis port
 
 ###❖➥
-PASSWORD = ""  # redis password
+PASSWORD = "ox6hcmlxU3ZezlpjkOCeETnmGTxMoEXl"  # redis password
 
 ###❖➥
-PRIVATE_CHAT_ID = "-1002010924139"  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = "-1002385013996"  # CHAT WHERE YOU WANT TO STORE VIDEOS
 
 ###❖➥
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+COOKIE = "csrfToken=HvgtYMokku2V--2Mv-jFebIP; browserid=6Ivy3woJ7REjIR6mYgRWb7ERu1lbm93Kcv6YGwxOqDYtKo4Sd26q-IYx_68=; lang=en; TSID=DcnLf4FLmqzUj3GAFnxa8Pme7jaS3gAA; _ga=GA1.1.766589057.1740022350; _gcl_au=1.1.1634502234.1740022350; __bid_n=195216a02af49cb4ad4207; _fbp=fb.1.1740022350749.655428833194283917; _ga_K6JMPYL99R=GS1.1.1740022349.1.0.1740022425.0.0.0; _ga_RSNVN63CM3=GS1.1.1740022350.1.0.1740022425.60.0.0; ab_sr=1.0.1_YTcxZGYzNTgzMzczYjBhMGI0MmRmNDgyMmIyZTlkMWNkYjA3MWE4MjI3NTk2NDYxNzg3MGQzYzA3MmIzNzVlODFhNDViZTg3YWE2NzY5YmU0ZjFmMTlmNjJiYTkxOGJmMjQwNTk1M2RlZjU1MDM2Nzc2NmJjZTgwMmRmYTA3OGNhMmJkZWU1NWFkYTE0YWYxZmQyY2UzMDY4NTlmODE0OQ==; ndut_fmt=FC1D7B97BA1313B5617D96ECBF714259C8426C8919D6F7DDBA007EB018BFDCB2"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
 ###❖➥
-ADMINS = "6922271843"
+ADMINS = "6077444526"
